2ccb3f8fa64daa173e9e567f0889c93816167b7c
kimari david <kimaridavid8@gmail.com>
2025-12-01 12:00:13 +0300

    javascript-scope-lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your solution in this file!
+const burgers = ['hamburger', 'cheeseburger'];
+let featureDrink = 'strawberry Milkshake';
+
+function addBurger() {
+
+    const newBurger = 'Flatburger';
+    burgers.push(newBurger);
+}
+
+if(true) {
+    const anotherNewBurger =  'Maple Bacon Burger';
+    burgers.push(anotherNewBurger);
+}
+function changeFeatureDrink() {
+    featureDrink = 'The javashaker';
+}
